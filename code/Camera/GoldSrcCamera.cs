9f3e3e325697bb87b6c43b2300c47207a26bb9b8
Selim Nahimi <selimnahimi@gmail.com>
2022-08-01 18:11:54 +0200

    View bobbing

--- code/Camera/GoldSrcCamera.cs
+++ code/Camera/GoldSrcCamera.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace Sandbox
 {
 	public class GoldSrcCamera : CameraMode
@@ -54,10 +56,55 @@ namespace Sandbox
 			VerticalRotationAmount *= invdelta;
 			HorizontalRotationAmount *= invdelta;
 
-			//Position += Time.Delta * 10 * invdelta * Speed;
-
 			Rotation *= Rotation.FromAxis( Vector3.Right, VerticalRotationAmount );
 			Rotation *= Rotation.FromAxis( Vector3.Up, HorizontalRotationAmount );
+
+			
+			// Viewbob
+
+			var playerVelocity = Local.Pawn.Velocity;
+
+			if ( Local.Pawn is Player player )
+			{
+				var controller = player.GetActiveController();
+				if ( controller != null && controller.HasTag( "noclip" ) )
+				{
+					playerVelocity = Vector3.Zero;
+				}
+			}
+
+			float cl_bob = 0.01f;
+			float cl_bobcycle = 0.8f;
+			float cl_bobup = 0.5f;
+			float offset = 2f;
+
+			float cltime = Time.Now;
+			float cycle = (float)(cltime - Math.Floor( cltime / cl_bobcycle ) * cl_bobcycle);
+			cycle = cycle / cl_bobcycle;
+
+			if ( cycle < cl_bobcycle )
+			{
+				cycle = (float)(Math.PI * cycle / cl_bobup);
+			}
+			else
+			{
+				cycle = (float)(Math.PI + Math.PI * (cycle - cl_bobup) / (1.0 - cl_bobup));
+			}
+
+			float bob = (float)(Math.Sqrt( playerVelocity[0] * playerVelocity[0] + playerVelocity[1] * playerVelocity[1] ) * cl_bob);
+
+			bob = (float)(bob * 0.3 + bob * 0.7 * Math.Sin( cycle ));
+
+			if ( bob > 4 )
+			{
+				bob = 4;
+			}
+			else if ( bob < -7 )
+			{
+				bob = -7;
+			}
+
+			Position = Position + Rotation.Up * bob * 1f;
 		}
 
 		public void Shake( float length = 0.1f, float verticalRotation = 0.6f, float horizontalRotation = 0.6f )
