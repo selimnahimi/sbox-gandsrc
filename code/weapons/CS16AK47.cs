804adef6fd9b2b3812c623d0b4d98bc1dcb8363d
Selim Nahimi <selimnahimi@gmail.com>
2022-08-01 17:39:58 +0200

    Bigger shake for AK47

--- code/weapons/CS16AK47.cs
+++ code/weapons/CS16AK47.cs
@@ -42,7 +42,7 @@ namespace Sandbox
 			(Owner as AnimatedEntity)?.SetAnimParameter( "b_attack", true );
 
 			Particles.Create( "particles/pistol_muzzleflash.vpcf", EffectEntity, "muzzle" );
-			ShootEffects();
+			ShootEffects(shakeLength: 1.5);
 			PlayFireSound( "cs16-weapons-ak47-fire" );
 			ShootBullet( 0.05f, 1.5f, 9.0f, 3.0f );
 		}
